cda7b9f9d9770ef69ea560091c8f9a5575730f76
gqq <gqq519@163.com>
2016-12-01 16:49:46 +0800

    完善侧滑

--- app/src/main/java/com/feicuiedu/gitdroid/MainActivity.java
+++ app/src/main/java/com/feicuiedu/gitdroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.feicuiedu.gitdroid;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.feicuiedu.gitdroid.commons.ActivityUtils;
+import com.feicuiedu.gitdroid.login.LoginActivity;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -19,11 +23,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
     @BindView(R.id.navigationView)
-    NavigationView mNavigationView;
+    NavigationView mNavigationView;// 侧滑菜单
     @BindView(R.id.drawerLayout)
-    DrawerLayout mDrawerLayout;
+    DrawerLayout mDrawerLayout;// 抽屉效果(侧滑，里面包括两个布局：1. 主页面显示，2. 侧滑的布局，一般结合NavigationView使用)
     private Button mBtnLogin;
     private ImageView mIvIcon;
+    private ActivityUtils mActivityUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +43,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public void onContentChanged() {
         super.onContentChanged();
+        mActivityUtils = new ActivityUtils(this);
         ButterKnife.bind(this);
 
         /**
          * 需要处理的视图
-         * 1. toolbar
-         * 2. DrawerLayout
+         * 1. toolbar：主题是没有ActionBar，所以展示的时候toolbar作为actionBar展示
+         * 2. DrawerLayout：
          * 3. NavigationView
-         *
          */
 
         //设置ActionBar
@@ -53,7 +58,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         // 设置监听
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,mDrawerLayout,mToolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
-        toggle.syncState();
+        toggle.syncState();// 同步状态
         // 设置DrawerLayout的侧滑监听
         mDrawerLayout.addDrawerListener(toggle);
 
@@ -64,7 +69,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: 2016/12/1 跳转到登录页面
+                mActivityUtils.startActivity(LoginActivity.class);
+                finish();
             }
         });
     }
@@ -73,7 +79,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onStart() {
         super.onStart();
-
+        // TODO: 2016/12/1 展示登录用户的信息
     }
 
     @Override
@@ -90,7 +96,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             // 开发者
             case R.id.github_hot_coder:
                 break;
+            // 我的收藏
+            case R.id.arsenal_my_repo:
+                break;
+            // 每日干货
+            case R.id.tips_daily:
+                break;
         }
-        return false;
+
+        // 选择某一项之后，切换Fragment，关闭抽屉
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+
+        // 返回true,代表该菜单项已经被选择
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
     }
 }
