68cd80ea2c3979ffc785230cc51848f4f9a71d28
gqq <gqq519@163.com>
2016-12-02 14:28:04 +0800

    热门仓库布局搭建

---
+++ app/src/main/java/com/feicuiedu/gitdroid/github/HotRepoAdapter.java
@@ -0,0 +1,26 @@
+package com.feicuiedu.gitdroid.github;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by gqq on 2016/12/2.
+ */
+
+public class HotRepoAdapter extends FragmentPagerAdapter{
+
+    public HotRepoAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new RepoListFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 10;
+    }
+}
